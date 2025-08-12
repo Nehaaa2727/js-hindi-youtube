efaadaf224af93b26bc2d994dd3f7627cb2b3987
Nehaaa2727 <167599154+Nehaaa2727@users.noreply.github.com>
2025-08-12 10:07:37 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "44abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN  means not a number 
+
+// "33" --> 33
+// "33abc" --> NaN
+//  true --> 1; false --> 0
+
+let isLoggeIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggeIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 --> true, 0 --> false
+//  "" --> true
+//  "neha" --> true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
