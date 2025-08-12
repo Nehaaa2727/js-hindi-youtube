c24f6f01963da287af99a8eb3c6fcfb00692ca2f
Nehaaa2727 <167599154+Nehaaa2727@users.noreply.github.com>
2025-08-12 09:48:56 +0000

    learn about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+ console.log(3 + 3);  //code readability should be high
+
+
+ let name = "neha" // string
+ let age = 21 // number
+ let isloggedIn = true // boolean
+ let state;
+
+
+//  number --> 2 to power 53
+//  bigint 
+// string --> ""
+//  boolean --> true/false
+//  null --> stanlone value
+//  undefined --> variable is not defined
+//  symbol --> unique identifier
+
+//  objects
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
+
+console.log(typeof "neha");
